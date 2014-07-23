c75f0651631a0374ff1a9cc6739b77fa86abbff2
Allwyn <allwyn@fafadiatech.com>
2014-07-23 11:36:39 +0530

    load show method code added

--- ShowTracker/ShowTracker/ViewController.m
+++ ShowTracker/ShowTracker/ViewController.m
@@ -8,8 +8,15 @@
 
 #import "ViewController.h"
 #import "TraktAPIClient.h"
+#import <AFNetworking/UIKit+AFNetworking.h>
+#import <Nimbus/NIAttributedLabel.h>
+#import <SAMCategories/UIScreen+SAMAdditions.h>
 
 @interface ViewController ()
+
+@property (nonatomic, strong) NSArray *jsonResponse;
+@property (nonatomic) BOOL pageControlUsed;
+@property (nonatomic) NSInteger previousPage;
 
 @end
 
@@ -20,6 +27,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.previousPage = -1;
     
     TraktAPIClient *client = [TraktAPIClient sharedClient];
     
@@ -27,7 +35,23 @@
                    username:@"rwtestuser"
                numberOfDays:3
                     success:^(NSURLSessionDataTask *task, id responseObject) {
-                        NSLog(@"Success -- %@", responseObject);
+                        // Save response object
+                        self.jsonResponse = responseObject;
+                        
+                        // Get the number of shows
+                        NSInteger shows = 0;
+                        for (NSDictionary *day in self.jsonResponse)
+                            shows += [day[@"episodes"] count];
+                        
+                        // Set up page control
+                        self.showsPageControl.numberOfPages = shows;
+                        self.showsPageControl.currentPage = 0;
+                        
+                        // Set up scroll view
+                        self.showsScrollView.contentSize = CGSizeMake(CGRectGetWidth(self.view.bounds) * shows, CGRectGetHeight(self.showsScrollView.frame));
+                        
+                        // Load first show
+                        [self loadShow:0];
                     }
                     failure:^(NSURLSessionDataTask *task, NSError *error) {
                         NSLog(@"Failure -- %@", error);
@@ -41,4 +65,45 @@
     
 }
 
+- (void)loadShow:(NSInteger)index
+{
+    // 1 - Find the show for the given index
+    NSDictionary *show = nil;
+    NSInteger shows = 0;
+    
+    for (NSDictionary *day in self.jsonResponse) {
+        NSInteger count = [day[@"episodes"] count];
+        
+        // 2 - Did you find the right show?
+        if (index < shows + count) {
+            show = day[@"episodes"][index - shows];
+            break;
+        }
+        
+        // 3 - Increment the shows counter
+        shows += count;
+    }
+    
+    // 4 - Load the show information
+    NSDictionary *showDict = show[@"show"];
+    
+    // 5 - Display the show title
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(index * CGRectGetWidth(self.showsScrollView.bounds) + 20, 40, CGRectGetWidth(self.showsScrollView.bounds) - 40, 40)];
+    titleLabel.text = showDict[@"title"];
+    titleLabel.backgroundColor = [UIColor clearColor];
+    titleLabel.textColor = [UIColor whiteColor];
+    titleLabel.font = [UIFont boldSystemFontOfSize:18];
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    
+    // 6 - Add to scroll view
+    [self.showsScrollView addSubview:titleLabel];
+}
+
 @end
+
+
+
+
+
+
+
